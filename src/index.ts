cc403e855e69e8e997419c6592dfe6d176bb3940
Ayushi Chouhan <ayushi.chouhan@example.com>
2025-12-26 18:48:08 +0530

    feat: implement WCI standard HTTP client with demo and tests

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { buildHttpErrorCodes } from './errors/errorCodes';
+import { get } from './requests/get';
+import { post } from './requests/post';
+
+export function createHttpClient(config?: { errorPrefix?: string }) {
+  const errorCodes = buildHttpErrorCodes(config?.errorPrefix);
+
+  return {
+    get,
+    post,
+    errorCodes,
+  };
+}
